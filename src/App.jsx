b66e295cdb6d043d9e915451fd9ec8afd0455335
draj812602 <draj.8126@gmail.com>
2025-03-01 21:14:00 +0530

    Added string calculator function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import StringCalculator from "./components/StringCalculator";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <div>
+      <h1>String calculator</h1>
+    </div>
   );
 }
 
---
+++ src/components/StringCalculator.js
@@ -0,0 +1,19 @@
+export const add = (numbers) => {
+  if (!numbers) return 0;
+
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].substring(2)); // Extract custom delimiter
+    numbers = parts[1];
+  }
+
+  const numArray = numbers.split(delimiter).map(Number);
+  const negatives = numArray.filter((num) => num < 0);
+
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+  }
+
+  return numArray.reduce((sum, num) => sum + num, 0);
+};
